0da0104a5dc83c5be3e8827c8ebba171708355ed
yogajones <95644662+yogajones@users.noreply.github.com>
2024-07-05 13:31:31 +0300

    Ex 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,18 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  res.send(req.todo);
+})
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body;
+
+  if (text !== undefined) req.todo.text = text;
+  if (done !== undefined) req.todo.done = done;
+
+  await req.todo.save();
+  res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
